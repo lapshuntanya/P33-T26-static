cacb550a4e1d82c75028e15b92391e543f5085b3
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-18 17:28:39 +0300

    add

--- P33-T26-static/CarNumber.cpp
+++ P33-T26-static/CarNumber.cpp
@@ -1,1 +1,48 @@
 #include "CarNumber.h"
+
+CarNumber::CarNumber()
+{
+    counter++;
+    if (counter > 9999) {
+        counter = 1;
+        secondLetter++;
+        if (secondLetter > 'Z') {
+            secondLetter = 'A';
+            firstLetter++;
+        }
+    }
+
+    number = to_string(counter / 1000) + to_string(counter / 100 % 10) +
+        to_string(counter / 10 % 10) + to_string(counter + 10)
+        + firstLetter + secondLetter;
+}
+
+string CarNumber::getNumber() const{    
+    return number;
+}
+
+int CarNumber::getCounter() {
+    return counter;
+}
+
+char CarNumber::getFirstLetter() {
+    return firstLetter;
+}
+
+char CarNumber::getSecondLetter() {
+    return secondLetter;
+}
+
+void CarNumber::setCounter(int n) {
+    counter = n;
+}
+
+void CarNumber::setFirst(char a) {
+    firstLetter = a;
+}
+
+void CarNumber::setSecond(char b) {
+    secondLetter = b;
+}
+
+ 
